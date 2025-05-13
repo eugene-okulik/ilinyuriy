be98c9c6ffa66b0414119591834e1cd366328f2b
Ilin-Yuriy <IlinYuriy1908@gmail.com>
2025-05-13 22:15:12 +0800

    ДЗ №6 Conditions & Loops

---
+++ homework/Yuriy_Ilin/Homework_6/Задание 1.py
@@ -0,0 +1,14 @@
+s = ('''Etiam tincidunt neque erat, quis molestie enim imperdiet vel. 
+Integer urna nisl, facilisis vitae semper at, dignissim vitae libero''')
+
+lst_words = s.split()
+
+lst_mod = []
+for i in lst_words:
+    if i[-1] not in ".,":  #Можно расширить список символов, которые должны быть оставлены в конце слова
+        lst_mod.append(i + 'ing')
+    else:
+        lst_mod.append(i[:-1] + 'ing' + i[-1])
+
+print(' '.join(lst_mod))
+
